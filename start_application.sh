5d816a254c9690479592e166b27b6c9de74a08b5
Aritra Sur Roy <aritra1997@hotmail.com>
2025-09-13 17:07:22 +0530

    add start application script

---
+++ start_application.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+make build
+
+SERVICE_NAME=opentelemetry-demo \
+    INSECURE_MODE=false \
+    OTEL_EXPORTER_OTLP_HEADERS=signoz-ingestion-key=<signoz ingestion key> \
+    OTEL_EXPORTER_OTLP_ENDPOINT=ingest.<region>.signoz.cloud:443 \
+    app
