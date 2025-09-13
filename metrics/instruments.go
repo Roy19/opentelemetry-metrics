813b2aefc8f411b796c669f614d19b4f828f1956
Aritra Sur Roy <aritra1997@hotmail.com>
2025-09-13 09:55:08 +0000

    add request duration histogram

--- metrics/instruments.go
+++ metrics/instruments.go
@@ -12,6 +12,7 @@ import (
 var (
 	successfulRequests metric.Int64Counter
 	failedRequests     metric.Int64Counter
+	requestDuration    metric.Float64Histogram
 )
 
 func InitMeters() {
@@ -23,14 +24,22 @@ func InitMeters() {
 		metric.WithDescription("Total number of successful requests"),
 	)
 	if err != nil {
-		log.Fatalf("Failed to create successful_requests_total counter: %v", err)
+		log.Fatalf("failed to create successful_requests_total counter: %v", err)
 	}
 	failedRequests, err = meter.Int64Counter(
 		"failed_requests_total",
 		metric.WithDescription("Total number of failed requests"),
 	)
 	if err != nil {
-		log.Fatalf("Failed to create failed_requests_total counter: %v", err)
+		log.Fatalf("failed to create failed_requests_total counter: %v", err)
+	}
+	requestDuration, err = meter.Float64Histogram(
+		"request_latency",
+		metric.WithDescription("Measures the latency of a HTTP method"),
+		metric.WithUnit("ms"),
+	)
+	if err != nil {
+		log.Fatalf("failed to create request_latency histogram: %v", err)
 	}
 }
 
@@ -43,5 +52,12 @@ func IncSuccessfulRequests(ctx context.Context, code int) {
 func IncFailedRequests(ctx context.Context, code int) {
 	failedRequests.Add(ctx, 1, metric.WithAttributes(
 		attribute.Int("http.status_code", code),
+	))
+}
+
+func RecordLatency(ctx context.Context, duration float64, method string, endpoint string) {
+	requestDuration.Record(ctx, duration, metric.WithAttributes(
+		attribute.String("http.method", method),
+		attribute.String("http.endpoint", endpoint),
 	))
 }
