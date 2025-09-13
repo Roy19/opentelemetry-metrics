bfcf2c48d129122df69c21d06757f09d848fdafd
Aritra Sur Roy <32236407+Roy19@users.noreply.github.com>
2025-09-13 17:17:48 +0530

    fix typo

--- start_application.sh
+++ start_application.sh
@@ -5,4 +5,4 @@ SERVICE_NAME=opentelemetry-demo \
     INSECURE_MODE=false \
     OTEL_EXPORTER_OTLP_HEADERS=signoz-ingestion-key=<signoz ingestion key> \
     OTEL_EXPORTER_OTLP_ENDPOINT=ingest.<region>.signoz.cloud:443 \
-    app
+    ./app
