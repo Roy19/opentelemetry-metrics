d1c14bd9dcde5761d8c1d7871e514b8b1f6926e7
Aritra Sur Roy <aritra1997@hotmail.com>
2025-09-13 10:02:35 +0000

    modify migrations

--- db/migrations/000001_create_cart_table.up.sql
+++ db/migrations/000001_create_cart_table.up.sql
@@ -1,6 +1,6 @@
 create table if not exists public.carts (
     id serial primary key,
-    name varchar(50) not null
+    name varchar(50) not null unique
 );
 
 create table if not exists public.items (
